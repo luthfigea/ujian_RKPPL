c2c6233d3c22d301ccf764179d7c4be405b27a72
praktikum <praktikum@if-unpas.org>
2015-05-09 08:30:45 +0700

    commit tes pangkat 2

---
+++ Documents/NetBeansProjects/ujian_rkppl/src/main/java/com/mycompany/ujian_rkppl/TestRunner.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ujian_rkppl;
+
+/**
+ *
+ * @author SB 604-22
+ */
+public class TestRunner {
+    
+}
